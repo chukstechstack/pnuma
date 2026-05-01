dde4ec93298f27586115f98084fefe2fc7cb5610
Chukwunyelu Kingsley Emmanuel <chuks.techstack@gmail.com>
2026-05-01 12:24:40 +0100

    updated render vite

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,8 +1,8 @@
-
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000",
+  // This automatically picks the right URL based on where the app is running
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:3000",
   withCredentials: true,
 });
 
