c8966f30092941deee9d131576e7a1bcced77e4c
Chukwunyelu Kingsley Emmanuel <chuks.techstack@gmail.com>
2026-05-03 17:38:49 +0100

    fix: updated CORS whitelist and fixed path-to-regexp crash

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -4,9 +4,18 @@ const api = axios.create({
   // If I'm on my laptop, use 3000. If I'm on Render, use the API link.
   baseURL: import.meta.env.DEV 
     ? "http://localhost:3000" 
-    : "https://onrender.com", 
+    : import.meta.env.VITE_API_URL , 
   withCredentials: true,
 });
 
 export default api;
 
+// import axios from "axios";
+
+// const api = axios.create({
+//   // This automatically picks the right URL based on where the app is running
+//   baseURL: import.meta.env.VITE_API_URL || "http://localhost:3000",
+//   withCredentials: true,
+// });
+
+// export default api;
